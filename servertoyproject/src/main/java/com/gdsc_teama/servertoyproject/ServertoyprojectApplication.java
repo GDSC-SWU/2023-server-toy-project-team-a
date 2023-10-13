8a85b8cebd9f4d2e7b2a09cd639941f1e4e1341e
this-soyoung <this.soyoung@gmail.com>
2023-10-14 03:03:24 +0900

    feat: Add Entity (#1)

---
+++ servertoyproject/src/main/java/com/gdsc_teama/servertoyproject/ServertoyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc_teama.servertoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServertoyprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServertoyprojectApplication.class, args);
+    }
+
+}
---
+++ servertoyproject/src/test/java/com/gdsc_teama/servertoyproject/ServertoyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc_teama.servertoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServertoyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
